008068ba545e5fad6bbe47f04a5cf430efd34a38
zhonglingsun <zhonglingsun@yxlink.com>
2018-06-28 09:46:51 +0800

    create pom.xml

---
+++ demo/src/main/java/Controller/Example.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by speedjive on 2018/6/14.
+ */
+@RestController
+@SpringBootApplication
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
